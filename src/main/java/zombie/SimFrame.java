1d4dc33ce24af3bf55a18cc51a5398e6506d0bf8
megaboomers <megaboomers@gmail.com>
2026-02-20 14:02:07 -0800

    hello

--- src/main/java/zombie/SimFrame.java
+++ src/main/java/zombie/SimFrame.java
@@ -6,10 +6,6 @@ public class SimFrame extends JFrame {
     SimFrame() {
         this.add(new SimPanel());
         this.setTitle("Zombie");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(true);
-        this.pack();
-        this.setVisible(true);
-        this.setLocationRelativeTo(null);
+
     }
 }
